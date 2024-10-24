892a473b1a6df09efbfc20cd4ca8743b82684c7b
cz <435932323@qq.com>
2024-10-24 11:27:05 +0800

    feat: 增加UIDevice拓展

---
+++ Extension/UIDevice+Common.swift
@@ -0,0 +1,13 @@
+//
+//  UIDevice+Common.swift
+//  WuKongKit
+//
+//  Created by weitu on 2024/10/24.
+//
+
+import UIKit
+
+extension UIDevice {
+    
+    static let isPhone = UIDevice.current.userInterfaceIdiom == .phone
+}
