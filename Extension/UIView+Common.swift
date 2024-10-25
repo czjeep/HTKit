e8b864a148d5fc0b833714ac4be66a3ddc00fa31
cz <435932323@qq.com>
2024-10-25 16:34:03 +0800

    perf: 完善uiview.nib

--- Extension/UIView+Common.swift
+++ Extension/UIView+Common.swift
@@ -289,9 +289,13 @@ extension UIView {
     }
 }
 
+fileprivate class Goat {
+    
+}
+
 extension UIView {
     
     static func nib() -> UINib? {
-        return UINib(nibName: String(describing: self), bundle: nil)
+        return UINib(nibName: String(describing: self), bundle: Bundle(for: Goat.self))
     }
 }
