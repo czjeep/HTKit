3c17d4bd383281d9680b17369e19ecfa35bd0fc8
cz <435932323@qq.com>
2024-10-30 19:40:49 +0800

    feat: 增加FileManager对沙盒文件夹的拓展

---
+++ Extension/FileManager+Common.swift
@@ -0,0 +1,26 @@
+//
+//  FileManager+Common.swift
+//  WuKongKit
+//
+//  Created by weitu on 2024/10/30.
+//
+
+import Foundation
+
+extension FileManager {
+    
+    @objc public static let caches: String? = {
+        let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
+        return path.first
+    }()
+    
+    @objc public static let document: String? = {
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        return path.first
+    }()
+    
+    @objc public static let library: String? = {
+        let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)
+        return path.first
+    }()
+}
