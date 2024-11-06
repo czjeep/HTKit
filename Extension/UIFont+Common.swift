e3435c57a2121172fc7c422d61899ad6b0076b22
cz <435932323@qq.com>
2024-11-06 20:01:25 +0800

    fix: 调整字体对应的weight

--- Extension/UIFont+Common.swift
+++ Extension/UIFont+Common.swift
@@ -9,32 +9,37 @@ import UIKit
 
 extension UIFont {
     
-    /// 中粗体
+    /// 粗体 weight-700
+    static func pingFangBold(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Bold", size: size)!
+    }
+    
+    /// 中粗体 weight-600
     static func pingFangSemibold(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Semibold", size: size)!
     }
     
-    /// 中黑体
+    /// 中黑体 weight-500
     static func pingFangMedium(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Medium", size: size)!
     }
     
-    /// 常规体
+    /// 常规体 weight-400
     static func pingFangRegular(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Regular", size: size)!
     }
     
-    /// 细体
+    /// 细体 weight-300
     static func pingFangLight(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Light", size: size)!
     }
     
-    /// 纤细体
+    /// 纤细体 weight-200
     static func pingFangThin(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Thin", size: size)!
     }
     
-    /// 极细体
+    /// 极细体 weight-100
     static func pingFangUltralight(_ size: CGFloat) -> UIFont {
         UIFont(name: "PingFangSC-Ultralight", size: size)!
     }
