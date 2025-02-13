a73b5486732babc85a046e600ca0c943dda8ea4a
cz <435932323@qq.com>
2025-02-13 16:46:14 +0800

    feat: 增加时间矫正工具类-HTDateUtility

---
+++ HTDateUtility.swift
@@ -0,0 +1,30 @@
+//
+//  HTDateUtility.swift
+//  WuKongKit
+//
+//  Created by weitu on 2025/2/13.
+//
+
+import UIKit
+
+/// 时间矫正工具类
+class HTDateUtility: NSObject {
+    
+    ///差值。秒：正确时间戳-系统时间戳
+    static private var diffValue: Double = 0
+    
+    /// 矫正差值：正确时间戳毫秒
+    static func correctWithMilliseconds(_ timestamp: Double) {
+        correctWithSeconds(timestamp/1000)
+    }
+    
+    /// 矫正差值：正确时间戳秒
+    static func correctWithSeconds(_ timestamp: Double) {
+        diffValue = timestamp-Date.now.timeIntervalSince1970
+    }
+    
+    /// 当前时间
+    static var now: Date {
+        return Date(timeIntervalSinceNow: diffValue)
+    }
+}
