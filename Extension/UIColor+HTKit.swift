7b16327ddfce70d0fd8d9a07150f94fba618039e
cz <435932323@qq.com>
2025-12-10 15:56:25 +0800

    feat: 增加-随机颜色

--- Extension/UIColor+HTKit.swift
+++ Extension/UIColor+HTKit.swift
@@ -103,4 +103,14 @@ extension UIColor {
         
         return rgb
     }
+    
+    /// 随机颜色
+    static func random() -> UIColor {
+        UIColor(
+            red:   .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue:  .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
 }
